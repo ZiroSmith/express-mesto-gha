a3af85a2b07f2af6755557c8f643597abe668a9d
Kizilov Andrey <irosmith@yandex.ru>
2023-07-10 16:05:46 +0300

    Rev3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Нет доступа' });
+    throw new AuthorizationError('Нет доступа');
   }
 
   const token = authorization.replace('Bearer ', '');
